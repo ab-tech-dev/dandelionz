257fdb9f6e961bf01acaf90f83893fa12a796adc
ab-tech-dev <joshua4ability@gmail.com>
2025-12-05 21:07:41 +0100

    Business admin logic added to authentication/admin.py

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
 
-@admin.register(CustomUser)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'is_verified', 'role','created_at')
-    search_fields = ('email',)
-    list_filter = ('is_verified', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('full_name', 'phone_number', 'profile_picture')}),
+        ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'created_at', 'updated_at')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'role', 'is_staff', 'is_superuser'),
+        }),
+    )
+
+    ordering = ('email',)
+
+    def save_model(self, request, obj, form, change):
+        raw_password = form.cleaned_data.get("password")
+
+        # HASH THE PASSWORD before saving
+        if raw_password and not change:
+            obj.set_password(raw_password)
+
+        super().save_model(request, obj, form, change)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,35 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ('email', 'role', 'is_staff', 'is_active')
-    list_filter = ('role', 'is_staff', 'is_active')
-
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('full_name', 'phone_number', 'profile_picture')}),
-        ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser')}),
-        ('Important dates', {'fields': ('last_login', 'created_at', 'updated_at')}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'role', 'is_staff', 'is_superuser'),
-        }),
-    )
-
-    ordering = ('email',)
-
-    def save_model(self, request, obj, form, change):
-        raw_password = form.cleaned_data.get("password")
-
-        # HASH THE PASSWORD before saving
-        if raw_password and not change:
-            obj.set_password(raw_password)
-
-        super().save_model(request, obj, form, change)
-
-admin.site.register(CustomUser, CustomUserAdmin)
+# Register your models here.
