5becf0d222415a65c6c08ad90c05f19d24e3d12c
ab-tech-dev <joshua4ability@gmail.com>
2025-12-24 15:22:51 +0100

    repaired admin error

--- e_commerce_api/settings.py
+++ e_commerce_api/settings.py
@@ -6,10 +6,10 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 # Build paths inside the project
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = os.getenv('SECRET_KEY')
